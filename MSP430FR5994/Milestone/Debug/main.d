99a39bee0717aea222240ed2887f055d6f85be09
Veronica Williams <williamsv4@students.rowan.edu>
2017-10-14 11:49:08 -0400

    uploaded code

---
+++ MSP430FR5994/Milestone/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Embedded/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: D:/Embedded/ccsv7/ccs_base/msp430/include/msp430fr5994.h
+main.obj: D:/Embedded/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: D:/Embedded/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include/intrinsics.h
+main.obj: D:/Embedded/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include/intrinsics_legacy_undefs.h
+main.obj: D:/Embedded/ccsv7/ccs_base/msp430/include/legacy.h
+
+../main.c: 
+D:/Embedded/ccsv7/ccs_base/msp430/include/msp430.h: 
+D:/Embedded/ccsv7/ccs_base/msp430/include/msp430fr5994.h: 
+D:/Embedded/ccsv7/ccs_base/msp430/include/in430.h: 
+D:/Embedded/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include/intrinsics.h: 
+D:/Embedded/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include/intrinsics_legacy_undefs.h: 
+D:/Embedded/ccsv7/ccs_base/msp430/include/legacy.h: 
